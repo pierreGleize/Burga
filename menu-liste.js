5d5cb0ae578033d86e1270c20a0e1ed9a813c5ea
Your Name <you@example.com>
2024-07-04 18:49:51 +0200

    #1 header en cours

---
+++ menu-liste.js
@@ -0,0 +1,9 @@
+let  btnMenu=document.getElementById('btn-menu')
+let menu =document.querySelector('.menu')
+const boutonClose=document.querySelector('.boutonClose')
+btnMenu.addEventListener('click',()=>{
+    menu.classList.toggle('open')
+})
+boutonClose.addEventListener('click',()=>{
+    menu.classList.toggle('open')
+})
