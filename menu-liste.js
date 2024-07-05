607d1340961d48a5e649206d22b615dd9e987a1a
Your Name <you@example.com>
2024-07-05 11:40:20 +0200

    #2 je commence le panier

--- menu-liste.js
+++ menu-liste.js
@@ -3,6 +3,7 @@ let menu =document.querySelector('.menu')
 const boutonClose=document.querySelector('.boutonClose')
 btnMenu.addEventListener('click',()=>{
     menu.classList.toggle('open')
+   
 })
 boutonClose.addEventListener('click',()=>{
     menu.classList.toggle('open')
