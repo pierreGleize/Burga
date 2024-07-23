5d58f2971e54305723ee3ba82d9bc59cbce70418
Your Name <you@example.com>
2024-07-23 22:15:56 +0200

    #16 retouche accueil et padding slider1

--- script.js/boutonMenuPanier.js
+++ script.js/boutonMenuPanier.js
@@ -6,7 +6,9 @@ const body = document.querySelector(".contenair");
 const btnPanier = document.querySelector(".btn-quantité");
 const panierPop = document.querySelector(".panier");
 const btnPanierClose = document.querySelector(".panier-close");
-
+const btnLoupe = document.getElementById("btn-loupe");
+const recherche = document.querySelector(".recherche");
+const rechercheClose = document.getElementById("recherche-close");
 btnMenu.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
@@ -26,3 +28,11 @@ btnPanierClose.addEventListener("click", () => {
 explorerProduit.addEventListener("click", () => {
   panierPop.classList.toggle("open");
 });
+
+btnLoupe.addEventListener("click", () => {
+  recherche.classList.toggle("ouvert");
+});
+rechercheClose.addEventListener("click", () => {
+  recherche.classList.toggle("ouvert");
+});
+//
